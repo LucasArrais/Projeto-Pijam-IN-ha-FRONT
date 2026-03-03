b556cdeeb0eb8b85773d5b02a8cca7aca76a3f9e
jpferrazoliv-code <jpferraz.oliv@gmail.com>
2026-03-03 16:42:35 -0300

    feat: add Header e Footer

---
+++ FrontEnd/src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+export default function Footer() {
+    return (
+        <div className={styles.Container}>
+
+
+        
+        </div>      // Container
+    )
+}
