d500c4bd53bb88d97647d699641e6528221f1fc7
jpferrazoliv-code <jpferraz.oliv@gmail.com>
2026-03-03 23:54:30 -0300

    feat: logo que leva para pag inicial

--- FrontEnd/src/components/header/index.tsx
+++ FrontEnd/src/components/header/index.tsx
@@ -3,6 +3,10 @@ import styles from './styles.module.css'
 export default function Header() {
   return (
     <header className={styles.container}>
+
+      <a href="#" className={styles.logo}>
+        <img src="/imagens_header/logo.png" alt="Página inicial" />
+      </a>
     </header>
   )
 }
