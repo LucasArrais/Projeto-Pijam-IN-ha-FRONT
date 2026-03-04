ce18855e55edaefde18c98f42f78cc8de11ca637
jpferrazoliv-code <jpferraz.oliv@gmail.com>
2026-03-03 23:55:46 -0300

    feat: add catalogo

--- FrontEnd/src/components/header/index.tsx
+++ FrontEnd/src/components/header/index.tsx
@@ -7,6 +7,13 @@ export default function Header() {
       <a href="#" className={styles.logo}>
         <img src="/imagens_header/logo.png" alt="Página inicial" />
       </a>
+
+      <div className={styles.catalogo}>
+        <a href="#">PIJAMAS</a>
+        <a href="#">FEMININO</a>
+        <a href="#">MASCULINO</a>
+        <a href="#">INFANTIL</a>
+      </div>
     </header>
   )
 }
