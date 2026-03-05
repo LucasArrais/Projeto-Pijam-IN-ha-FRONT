5fbc27921798679aa14fbb0949e9959320047bb7
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-05 13:21:47 +0100

    feat(/src/RootLayout.tsx): criação de arquivo RootLayout para que o header e o footer permaneçam em todas as páginas e haja uma renderização apenas da main de cada página

--- FrontEnd/src/router.tsx
+++ FrontEnd/src/router.tsx
@@ -1,23 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import PijamasPage from "./pages/PijamasPage";
+import RootLayout from "./RootLayout";
 
 const router = createBrowserRouter([
-    {   //nosso site vai se iniciar pela home, quando fizer a home, descomenta isso para funcionar a rota.
-        //path: "/",
-        //element: <Home />
-    },
     {
-        path: "/pijamas",
-        element: <PijamasPage />
-    },
-    {
-        //path: "/carrinho"
-        //element: <nome da função exportada da página carrinho>
-    },
-    {
-        //path: "/"
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            {
+                //nosso site vai se iniciar pela home, quando fizer a home, descomenta isso para funcionar a rota.
+                //index: true,
+                //element: <Home /> --nao esqueça de importar a funçao e nome deve ser igual a do index.tsx da HomePage
+            },
+            {
+                path: "/pijamas",
+                element: <PijamasPage />
+            },
+            {
+                //path: "/carrinho"
+                //element: <nome da função exportada da página carrinho>
+            },
+            {
+                //path: "/exemplo"
+                //element: <PageExmp /> 
+            }
+        ]
     }
-
 ])
 
 export default router;
