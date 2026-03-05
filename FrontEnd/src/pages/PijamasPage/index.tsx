d6f4c731d1e69ade7224c933ada38825f105a879
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-05 18:00:02 +0100

    feat(/pijamasPage/index.ts): estruturação html da pagina de pijamas

--- FrontEnd/src/pages/PijamasPage/index.tsx
+++ FrontEnd/src/pages/PijamasPage/index.tsx
@@ -1,10 +1,35 @@
-//aqui vai entrar a página que vai mostrar todos os pijamas 
-
+import styles from "./styles.module.css"
 
 export default function PijamasPage() {
     return (
         <>
-            <h2>teste</h2>
+        <div className={styles.background}>
+            <section className={styles.searchAndFilters}>
+                <div className={styles.searchbarAndLupa}>
+                    <input type="text" placeholder="Pesquise pelo produto..." />
+                    <button className={styles.searchButton}></button>
+                </div>
+                <div className={styles.filtersArea}>
+                    <button className={styles.genderButton}></button>
+                    <button className={styles.typeButton}></button>
+                    <button className={styles.seasonButton}></button>
+                </div>
+            </section>
+                
+            <section className={styles.pijmaDisplayAndPagination}>
+                    <div className={styles.pijamasDisplay}>
+                         
+                        <div className={styles.cardPijama}>
+                            <div></div>
+                        </div>
+                    </div>
+
+                    <div className={styles.pagination}>
+
+                    </div>
+            </section>
+        </div>
+                
         </>
     )
 }
