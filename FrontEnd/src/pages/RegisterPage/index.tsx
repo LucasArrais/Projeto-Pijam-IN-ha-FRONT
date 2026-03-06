6a2caa5171fc52addc2223734be53e55f9f3dd40
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-06 17:52:32 +0100

    fix(/pages/RegisterPage): correção das mensagens de span

--- FrontEnd/src/pages/RegisterPage/index.tsx
+++ FrontEnd/src/pages/RegisterPage/index.tsx
@@ -6,10 +6,10 @@ import axios from 'axios'
 
 const userSchema = z.object({
     name: z.string().nonempty().regex(/^\D+$/, {message: '* Não pode ter números'}),
-    username: z.string().nonempty('* O usuário não pode ser vazio').refine(value => value.trim().length > 0, { message: '* Não pode ter espaços' }).regex(/^[a-zA-Z0-9]+$/, {message: '* Não pode ter acentos'}),
+    username: z.string().nonempty('* Username não pode ser vazio').refine(value => value.trim().length > 0, { message: '* Não pode ter espaços' }).regex(/^[a-zA-Z0-9]+$/, {message: '* Não pode ter acentos'}),
     email: z.string().nonempty('* O E-mail não pode ser vazio').refine(value => z.string().safeParse(value).success, {message: '* O e-mail não é válido'}),
-    password: z.string().nonempty('Senha não pode ser vazia').min(6, "A senha deve ter no mínimo 6 caracteres").regex(/^\S+$/, "A senha Não pode conter espaços"),
-    confirmPassword: z.string().nonempty('A confirmação de senha não pode ser vazia')
+    password: z.string().nonempty('* Senha não pode ser vazia').min(6, "* A senha deve ter no mínimo 6 caracteres").regex(/^\S+$/, "* A senha Não pode conter espaços"),
+    confirmPassword: z.string().nonempty('* A confirmação de senha não pode ser vazia')
 }).refine(data => data.password === data.confirmPassword, {
     message: 'As senhas não cincidem',
     path: ['confirmPassword']
@@ -59,27 +59,27 @@ export default function RegisterPage() {
                             <input type="text" placeholder="Nome"
                             {...register('name')}
                             />
-                            {errors.name && <span>{errors.name.message}</span>}
+                            {errors.name && <span className={styles.span}>{errors.name.message}</span>}
                             <input type="text" placeholder="Nome de Usuário"
                             {...register('username')}
                             />
-                            {errors.username && <span>{errors.username.message}</span>}
+                            {errors.username && <span className={styles.span}>{errors.username.message}</span>}
                             <input type="email" placeholder="E-mail" 
                             {...register('email')}
                             />
-                            {errors.email && <span>{errors.email.message}</span>}
+                            {errors.email && <span className={styles.span}>{errors.email.message}</span>}
                             <input type="password" placeholder="Senha"
                             {...register('password')}
                             />
-                            {errors.password && <span>{errors.password.message}</span>}
+                            {errors.password && <span className={styles.span}>{errors.password.message}</span>}
                             <input type="password" placeholder="Confirmar senha"
                             {...register('confirmPassword')}
                             />
-                            {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+                            {errors.confirmPassword && <span className={styles.span}>{errors.confirmPassword.message}</span>}
                         </div>
 
                         <button className={styles.button}>{isSubmitting ? 'CARREGANDO...' : 'REGISTRAR'}</button>
-                        {errors.root && <span>{errors.root.message}</span>}
+                        {errors.root && <span className={styles.span}>{errors.root.message}</span>}
 
                     </form> 
                 </div>
