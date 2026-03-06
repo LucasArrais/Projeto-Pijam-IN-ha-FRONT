1160bd917738d202e0fd9fa38bbeaea5f9e4a161
jpferrazoliv-code <jpferraz.oliv@gmail.com>
2026-03-06 04:54:50 -0300

    feat: LoginCard

--- FrontEnd/src/pages/LoginPage/index.tsx
+++ FrontEnd/src/pages/LoginPage/index.tsx
@@ -3,6 +3,18 @@ import styles from "./styles.module.css"
 export default function LoginPage() {
     return (
         <div className={styles.Container}>
+            <div className={styles.LoginCard}>
+                <h1>Login</h1>
+
+                <p>Faça login para ter acesso ao <br/> pijama dos seus sonhos!</p>
+
+                <textarea className={styles.UsuarioEmail} placeholder="Usuário ou E-mail" />
+                <textarea className={styles.Senha} placeholder="Senha" />
+
+
+            <button className={styles.BotaoEntrar}>ENTRAR</button>
+            <button className={styles.BotaoCadastro}>CADASTRE-SE</button>
+            </div>
         </div>
     )
 }
