3112e4132ff0b8f7537120cda16bdfa13a58f5f3
jpferrazoliv-code <jpferraz.oliv@gmail.com>
2026-03-06 03:16:27 -0300

    feat: add txtarea e btn enviar

--- FrontEnd/src/pages/FeedbackPage/index.tsx
+++ FrontEnd/src/pages/FeedbackPage/index.tsx
@@ -8,6 +8,11 @@ export default function FeedbackPage() {
                 <h1>Feedback</h1>
 
                 <p>Fale um pouco sobre a sua <br/> experiência com a nossa loja!</p>
+
+                <textarea className={styles.NomeCompleto} placeholder="Nome Completo"     readOnly/>
+                <textarea className={styles.DescricaoDetalhada} placeholder="Descrição detalhada"     readOnly/>
+
+                <button>ENVIAR</button>
             </div>
         </div>
     )
