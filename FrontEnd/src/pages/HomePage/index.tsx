beb954ff11aeecb1853411a52c17eeae7d060157
jpferrazoliv-code <jpferraz.oliv@gmail.com>
2026-03-05 23:46:26 -0300

    feat: importando CSS e Swiper

---
+++ FrontEnd/src/pages/HomePage/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
